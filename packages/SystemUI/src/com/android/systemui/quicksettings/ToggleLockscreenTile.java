d46862c28587c11c5ea38287adda9fa7c5617540
gs2usr <jglarios1@gmail.com>
2013-08-25 06:33:56 -0300

    Port OnClick Fliptile animation.[1/2] credits to: TeamBAMF for the original work.
    Change-Id: I27cfd3871e45b8265e16fe15e4f4c1e964a18688
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/FChargeTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/RebootTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/ToggleLockscreenTile.java

--- packages/SystemUI/src/com/android/systemui/quicksettings/ToggleLockscreenTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/ToggleLockscreenTile.java
@@ -34,6 +34,10 @@ public class ToggleLockscreenTile extends QuickSettingsTile
                 sDisabledLockscreen = !sDisabledLockscreen;
                 mPrefs.edit().putBoolean(KEY_DISABLED, sDisabledLockscreen).apply();
                 updateLockscreenState();
+
+                if (isFlipTilesEnabled()) {
+                    flipTile(0);
+                }
             }
         };
 
